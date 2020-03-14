bc0fcfaee8be3d153f60038f84a7ac59db3bcb54
Dip-Ghosh <dipnsucse@gmail.com>
2020-03-14 23:11:28 +0600

    git const example

--- Complete Guide of Vue JS/ES6-14.03.2019/js/vue.js
+++ Complete Guide of Vue JS/ES6-14.03.2019/js/vue.js
@@ -1,6 +1,18 @@
-const type = "Cupcake";
-let flavor = "Apple";
-let price = 5;
+//introduction to const and let
+// const type = "Cupcake";
+// let flavor = "Apple";
+// let price = 5;
 
-flavor = "Mango";
-console.log(flavor);
+// flavor = "Mango";
+// console.log(flavor);
+
+//const and let real life example
+
+var divs = document.getElementsByTagName('div');
+for( let i = 0; i < divs.length; i++)
+{
+
+  divs[i].addEventListener('click',function(){
+    console.log(i);
+  })
+}
