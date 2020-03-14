8b18abd94ef6ce8762942a442d92b00d0c023e16
Dip-Ghosh <dipnsucse@gmail.com>
2020-03-14 23:18:57 +0600

    var to const and let

--- Complete Guide of Vue JS/ES6-14.03.2019/js/vue.js
+++ Complete Guide of Vue JS/ES6-14.03.2019/js/vue.js
@@ -1,13 +1,11 @@
 //introduction to const and let
-// const type = "Cupcake";
-// let flavor = "Apple";
-// let price = 5;
-
-// flavor = "Mango";
-// console.log(flavor);
+const type = "Cupcake";
+let flavor = "Apple";
+let price = 5;
+flavor = "Mango";
+console.log(flavor);
 
 //const and let real life example
-
 var divs = document.getElementsByTagName('div');
 for( let i = 0; i < divs.length; i++)
 {
@@ -16,3 +14,13 @@ for( let i = 0; i < divs.length; i++)
     console.log(i);
   })
 }
+
+//practice problems for changes var to const let
+const name = "James";
+const employeeId = 25;
+const eyes = "blue";
+let age = 36;
+let position = "Developer";
+let salary = 2500;
+let knowledge = ['java','python'];
+let married = false;
