be45503463b53623e051afa87331d90af311aa0b
Dip-Ghosh <dipnsucse@gmail.com>
2020-03-14 23:33:03 +0600

    End of ES6 lecture 121 done with exercise

--- Complete Guide of Vue JS/ES6-14.03.2019/js/vue.js
+++ Complete Guide of Vue JS/ES6-14.03.2019/js/vue.js
@@ -1,29 +1,29 @@
-// //introduction to const and let
-// const type = "Cupcake";
-// let flavor = "Apple";
-// let price = 5;
-// flavor = "Mango";
-// console.log(flavor);
+//introduction to const and let
+const type = "Cupcake";
+let flavor = "Apple";
+let price = 5;
+flavor = "Mango";
+console.log(flavor);
 
-// //const and let real life example
-// var divs = document.getElementsByTagName('div');
-// for( let i = 0; i < divs.length; i++)
-// {
+//const and let real life example
+var divs = document.getElementsByTagName('div');
+for( let i = 0; i < divs.length; i++)
+{
 
-//   divs[i].addEventListener('click',function(){
-//     console.log(i);
-//   })
-// }
+  divs[i].addEventListener('click',function(){
+    console.log(i);
+  })
+}
 
-// //practice problems for changes var to const let
-// const name = "James";
-// const employeeId = 25;
-// const eyes = "blue";
-// let age = 36;
-// let position = "Developer";
-// let salary = 2500;
-// let knowledge = ['java','python'];
-// let married = false;
+//practice problems for changes var to const let
+const name = "James";
+const employeeId = 25;
+const eyes = "blue";
+let age = 36;
+let position = "Developer";
+let salary = 2500;
+let knowledge = ['java','python'];
+let married = false;
 
 //practice problems
 let cupcake = document.querySelector('.cupcake');
